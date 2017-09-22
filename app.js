37dc5bdfc3cb708463fb895676ccf9cc8589c7b6
Frixolydian <mauricio.morales.arias93@gmail.com>
2017-09-22 16:08:23 -0500

    boop

---
+++ app.js
@@ -0,0 +1,159 @@
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client', express.static(__dirname + '/client'));
+
+serv.listen(process.env.PORT || 2000);
+console.log('Server started.')
+
+function checkDistance(a, b){
+  var dx = a.x - b.x;
+  var dy = a.y - b.y;
+  var dz = a.z - b.z;
+  return Math.sqrt(dx*dx+dy*dy+dz*dz);
+}
+
+var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+var ENEMY_LIST = {};
+
+var Player = function(id){
+	var self = {
+		x:Math.random() * 5 - 2.5,
+		y:Math.random() * 5 - 2.5,
+		z: 0,
+		id: id,
+	}
+	self.updatePosition = function(){
+		if (self.pressingRight){
+			self.x += 0.1
+		}
+		if (self.pressingLeft){
+			self.x -= 0.1
+		}
+		if (self.pressingUp){
+			self.y += 0.1
+		}
+		if (self.pressingDown){
+			self.y -= 0.1
+		}
+	}
+	return self;
+}
+
+var Enemy = function(){
+	var self = {
+		x:0,
+		y:0,
+		z:0,
+		id:id,
+		speedX:0,
+		speedY:0,
+		speedZ:0,
+	}
+}
+
+
+var Coin = function(){
+	var self = {
+		x:Math.random() * 4 - 2,
+		y:Math.random() * 4 - 2,
+		z: 0,
+	}
+	self.checkCollision = function(){
+		for (var i in PLAYER_LIST){
+		  if (checkDistance(PLAYER_LIST[i], self) < 0.3){
+		  	self.x = Math.random() * 4 - 2;
+		  	self.y = Math.random() * 4 - 2;
+				return true;
+			}
+		}
+	}
+	return self;
+}
+
+var coin = Coin();
+
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+	socket.id = Math.random();
+	var socket_id = socket.id;
+	SOCKET_LIST[socket.id] = socket;
+
+	var player = Player(socket.id);
+	PLAYER_LIST[socket.id] = player;
+
+	socket.emit('createCoin', coin);
+
+	console.log('socket connection ' + socket.id);
+
+	var createPlayers = {
+		list: PLAYER_LIST,
+		id: socket.id,
+	};
+	socket.emit('createPlayers', createPlayers);
+
+	socket.broadcast.emit('newPlayer', player);
+
+	socket.on('newConnection', function(){
+
+	})
+
+
+	socket.on('keyPress', function(data){
+		if (data.inputId === 'left'){
+			player.pressingLeft = data.state;
+		}
+		if (data.inputId === 'right'){
+			player.pressingRight = data.state;
+		}
+		if (data.inputId === 'up'){
+			player.pressingUp = data.state;
+		}
+		if (data.inputId === 'down'){
+			player.pressingDown = data.state;
+		}
+	})
+
+
+  socket.on('disconnect', function(){
+		delete SOCKET_LIST[socket.id];
+		delete PLAYER_LIST[socket.id];
+		console.log('disconnected ' + socket_id)
+		socket.broadcast.emit('usergone', {
+    	'left_user' : socket_id
+    });
+  })
+});
+
+
+setInterval(function(){
+	var pack = [];
+	for (var i in PLAYER_LIST){
+		var player = PLAYER_LIST[i];
+		player.updatePosition();
+		pack.push({
+			x:player.x,
+			y:player.y,
+			id:player.id
+		});
+	}
+
+
+
+	for (var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.emit('newPositions', pack);
+		if (coin.checkCollision()){
+			socket.broadcast.emit('coinGrabbed', coin);			
+		}
+	}
+	
+
+
+}, 1000/60);
---
+++ client/js/game.js
@@ -0,0 +1,355 @@
+var socket = io();
+var user = Math.random();
+
+var score = 0;
+
+
+var scoretext = document.createElement('div');
+scoretext.style.position = 'absolute';
+//text2.style.zIndex = 1;    // if you still don't see the label, try uncommenting this
+scoretext.style.width = 200;
+scoretext.style.height = 200;
+scoretext.style.color = "white";
+scoretext.innerHTML = "Score: " + score;
+scoretext.style.top = 20 + 'px';
+scoretext.style.left = 20 + 'px';
+document.body.appendChild(scoretext);
+
+function getDistance(mesh1, mesh2) { 
+  var dx = mesh1.position.x - mesh2.position.x; 
+  var dy = mesh1.position.y - mesh2.position.y; 
+  var dz = mesh1.position.z - mesh2.position.z; 
+  return Math.sqrt(dx*dx+dy*dy+dz*dz); 
+}
+
+var i;
+
+var scene = new THREE.Scene;
+
+var camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000 );
+camera.position.z = 5;
+camera.position.y = -5;
+
+var renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+renderer.setSize( window.innerWidth * 0.8, window.innerHeight * 0.8);
+document.body.appendChild( renderer.domElement );
+
+var ambient = new THREE.AmbientLight( 0x404040, 2 ); // soft white light
+scene.add( ambient );
+
+//Create a PointLight and turn on shadows for the light
+var light = new THREE.PointLight( 0xffffff, 0.5, 100 );
+light.position.set(1, -2, 3);
+light.castShadow = true;            // default false
+scene.add( light );
+
+//Set up shadow properties for the light
+light.shadow.mapSize.width = 1024;  // default
+light.shadow.mapSize.height = 1024; // default
+light.shadow.camera.near = 0.5;       // default
+light.shadow.camera.far = 1000      // default
+
+
+// Ground
+var ground_material = new THREE.MeshLambertMaterial({color: 0xaaaaaa});
+var ground = new THREE.Mesh(new THREE.BoxGeometry(5, 5, 0.1), ground_material);
+ground.position.z = -0.2;
+ground.receiveShadow = true;
+scene.add( ground );
+
+
+//createCoin();
+
+
+var players = {};
+
+selfCreated = false;
+
+PLAYER_LIST = [];
+
+var socketId;
+
+socket.on('createPlayers', function(data){
+	socketId = data.id;
+	for (var i in data.list){
+		console.log(data.list[i])
+		var cube_material = new THREE.MeshLambertMaterial({color:"red"});
+		if (data.list[i].id == data.id){
+			cube_material = new THREE.MeshLambertMaterial({color:"blue"});
+		}
+		var cube = new THREE.Mesh(new THREE.CubeGeometry(0.3, 0.3, 0.3), cube_material);
+		cube.castShadow = true;
+		cube.player = true
+		cube.position.x = data.list[i].x;
+		cube.position.y = data.list[i].y;
+		cube.position.z = 0;
+		cube.playerID = data.list[i].id;
+		console.log(data.list[i].id)
+		players[data.list.id] = cube;
+		if (data.list[i].id == data.id){
+			cube.special = true;
+		}
+		scene.add(cube);
+		console.log('aaa')
+	}
+});
+
+
+socket.on('newPlayer', function(data){
+	if (data.id !== socketId) {
+	var cube_material = new THREE.MeshLambertMaterial({color:"red"});
+	var cube = new THREE.Mesh(new THREE.CubeGeometry(0.3, 0.3, 0.3), cube_material);
+	cube.castShadow = true;
+	cube.player = true
+	cube.position.x = data.x;
+	cube.position.y = data.y;
+	cube.position.z = 0;
+	cube.playerID = data.id;
+	console.log(data.id)
+	players[data.id] = cube;
+	scene.add(cube);
+	}
+});
+
+
+socket.on('usergone', function(data){
+	console.log(data.left_user)
+	for (var i in players){
+		if (players[i].playerID === data.left_user){
+			scene.remove(players[i]);
+		}
+	}
+	scene.traverse(function(node) {
+		if (node instanceof THREE.Mesh){
+			if (node.player === true){
+				if (node.playerID === data.left_user){
+					scene.remove(node);
+				}
+			}
+		}
+	});
+})
+
+
+socket.on('newPositions', function(data){
+	scene.traverse(function(node) {
+		if (node instanceof THREE.Mesh){
+			if (node.player === true){
+				for (var i in data){
+					if (data[i].id === node.playerID){
+//							console.log('iii')
+							node.position.x = data[i].x;
+							node.position.y = data[i].y;
+					}						
+				}
+			}
+		}
+	});
+})
+	var coin;
+
+socket.on('createCoin', function(data){
+	var coin_material = new THREE.MeshLambertMaterial({color: 0xFFFF00});
+	var coin_geometry =	new THREE.CylinderGeometry(0.2, 0.2, 0.05, 30, 1, false, 0, 2 * Math.PI);
+	coin = new THREE.Mesh(coin_geometry, coin_material);
+	coin.position.x = data.x;
+	coin.position.y = data.y;
+	coin.position.z = 0.2;
+	ascend(coin);
+	coin.cube = true;
+	coin.rotationspeed.z = 0.05;
+	coin.castShadow = true;
+	coin.coin = true;
+	scene.add( coin );
+})
+
+socket.on('coinGrabbed', function(data){
+	coin.position.x = data.x;
+	coin.position.y = data.y;
+})
+
+
+
+var keys = [];
+keys.up = false;
+keys.left = false;
+keys.right = false;
+keys.down = false;
+keys.jump = false;
+
+
+
+function ascend(self){
+	self.speed = [];
+	self.speed.x = 0;
+	self.speed.y = 0;
+	self.speed.z = 0;
+	self.rotationspeed = [];
+	self.rotationspeed.x = 0;
+	self.rotationspeed.y = 0;
+	self.rotationspeed.z = 0;
+}
+
+
+function createEnemy(){
+	var enemy_geometry = new THREE.CubeGeometry(0.5, 0.5, 0.5);
+	var enemy_material = new THREE.MeshNormalMaterial();
+	var enemy = new THREE.Mesh(enemy_geometry, enemy_material);
+	ascend(enemy);
+	scene.add(enemy);
+	i = Math.ceil(Math.random() * 3)
+	switch(i) {
+    case 3:
+			enemy.position.y = 10;
+			enemy.position.x = Math.random() * 5 - 2.5;
+			enemy.speed.y = -0.05 * Math.random() - 0.03;
+      break;
+    case 2:
+			enemy.position.x = -10;
+			enemy.position.y = Math.random() * 5 - 2.5;
+			enemy.speed.x = 0.05 * Math.random() + 0.03;
+       break;
+    case 1:
+			enemy.position.x = 10;
+			enemy.position.y = Math.random() * 5 - 2.5;
+			enemy.speed.x = -0.05 * Math.random() - 0.03;
+	}
+
+	enemy.position.z = 0.1
+	enemy.cube = true;
+	enemy.receiveShadow = true;
+	enemy.castShadow = true;
+	enemy.enemy = true;
+}
+
+//			createEnemy();
+//			createEnemy();
+//			createEnemy();
+
+
+
+
+
+document.addEventListener("keyup", onDocumentKeyUp, false);
+function onDocumentKeyUp(event) {
+	var keyCode = event.which;
+	if (keyCode == 38) {
+		socket.emit('keyPress', {inputId: 'up', state: false})
+	}
+	if (keyCode == 40) {
+		socket.emit('keyPress', {inputId: 'down', state: false})
+	}
+	if (keyCode == 37) {
+		socket.emit('keyPress', {inputId: 'left', state: false})
+	}
+	if (keyCode == 39) {
+		socket.emit('keyPress', {inputId: 'right', state: false})
+	}
+	if (keyCode == 32) {
+			keys.jump = false;
+	}
+};
+
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+	var keyCode = event.which;
+	if (keyCode == 38) {
+		socket.emit('keyPress', {inputId: 'up', state: true})
+	}
+	if (keyCode == 40) {
+		socket.emit('keyPress', {inputId: 'down', state: true})
+	}
+	if (keyCode == 37) {
+		socket.emit('keyPress', {inputId: 'left', state: true})
+	}
+	if (keyCode == 39) {
+		socket.emit('keyPress', {inputId: 'right', state: true})
+	}
+	if (keyCode == 32) {
+		if (keys.jump == false && cube.position.z == 0){
+				cube.speed.z = 0.15;
+				keys.jump = true;
+		}
+	}
+};
+
+function jumping() {
+};
+
+function movement(){
+	scene.traverse(function(node) {
+		if (node instanceof THREE.Mesh){
+			if (node.cube){
+				node.position.x += node.speed.x;
+				node.position.y += node.speed.y;
+				node.position.z += node.speed.z;
+				node.rotation.x += node.rotationspeed.x;
+				node.rotation.y += node.rotationspeed.y;
+				node.rotation.z += node.rotationspeed.z;
+			}
+		}
+	});
+}
+
+
+
+function update(){
+
+
+
+
+/*
+	scene.traverse(function(node) {
+		if (node instanceof THREE.Mesh){
+			if (node.enemy){
+				if (node.position.x < -12 || node.position.x > 12 || node.position.y < -12){
+					scene.remove(node)
+					createEnemy()
+				}
+				if (getDistance(node, cube) < 0.4){
+					scene.remove(cube)
+					scoretext.innerHTML = "x.x moristeS, Score: " + score;
+				}
+			}
+			if (node.coin){
+				if (getDistance(node, cube) < 0.4){
+					score += 1;
+					scoretext.innerHTML = "Score: " + score;
+					scene.remove(node);
+					createCoin();
+					createEnemy();
+				}
+			}
+
+		}
+	});
+*/
+
+}
+
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+//	camera.lookAt(cube.position );
+	camera.rotation.z = 0;
+	for (var i in players)
+		scene.traverse(function(node) {
+			if (node instanceof THREE.Mesh){
+				if (node.special === true){
+					camera.lookAt(node.position);
+					camera.rotation.z = 0;
+				}
+			}
+		});
+//	updateMovement();
+
+
+
+	movement();
+	update();
+	jumping();
+};
+animate();
